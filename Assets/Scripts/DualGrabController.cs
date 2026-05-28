a3e54d6d5eb09f42d3470e3eef8f1aa7bc37b91e
fiorellacalvom <fiorella.calvo@estudiantec.cr>
2026-05-28 15:33:42 -0600

    cambios para probar

--- Assets/Scripts/DualGrabController.cs
+++ Assets/Scripts/DualGrabController.cs
@@ -1,101 +1,47 @@
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class DualGrabController : MonoBehaviour
 {
-    public XRGrabInteractable grabInteractable;
+    [Header("Scene Objects")]
+    public GameObject crossLegMannequin;
 
-    public Transform leftAttachPoint;
+    public GameObject ankleZone;
 
-    public Transform rightAttachPoint;
+    [HideInInspector]
+    public bool leftHandDetected = false;
 
-    private bool leftGrab = false;
+    [HideInInspector]
+    public bool rightHandDetected = false;
 
-    private bool rightGrab = false;
+    private bool alreadyActivated = false;
 
     void Start()
     {
-        // Bloqueado inicialmente
-        grabInteractable.trackPosition = false;
+        // Apagados al inicio
+        crossLegMannequin.SetActive(false);
 
-        grabInteractable.trackRotation = false;
-
-        grabInteractable.selectEntered.AddListener(OnGrab);
-
-        grabInteractable.selectExited.AddListener(OnRelease);
+        ankleZone.SetActive(false);
     }
 
-    void OnGrab(SelectEnterEventArgs args)
+    public void CheckBothHands()
     {
-        string tag = args.interactorObject.transform.tag;
-
-        // CONTROL IZQUIERDO
-        if(tag == "LeftController")
+        if(leftHandDetected && rightHandDetected)
         {
-            leftGrab = true;
+            Debug.Log("AMBAS MANOS DETECTADAS");
 
-            // SOLO si aún no hay otra mano
-            if(!rightGrab)
+            // Solo una vez
+            if(!alreadyActivated)
             {
-                grabInteractable.attachTransform = leftAttachPoint;
+                alreadyActivated = true;
+
+                // Activar maniquí
+                crossLegMannequin.SetActive(true);
+
+                // Activar zona tobillo
+                ankleZone.SetActive(true);
+
+                Debug.Log("MANIQUÍ Y TOBILLO ACTIVADOS");
             }
-
-            Debug.Log("LEFT HAND DETECTED");
-        }
-
-        // CONTROL DERECHO
-        if(tag == "RightController")
-        {
-            rightGrab = true;
-
-            // SOLO si aún no hay otra mano
-            if(!leftGrab)
-            {
-                grabInteractable.attachTransform = rightAttachPoint;
-            }
-
-            Debug.Log("RIGHT HAND DETECTED");
-        }
-
-        CheckBothHands();
-    }
-
-    void OnRelease(SelectExitEventArgs args)
-    {
-        string tag = args.interactorObject.transform.tag;
-
-        if(tag == "LeftController")
-        {
-            leftGrab = false;
-        }
-
-        if(tag == "RightController")
-        {
-            rightGrab = false;
-        }
-
-        // Si NO están ambas manos
-        if(!(leftGrab && rightGrab))
-        {
-            grabInteractable.trackPosition = false;
-
-            grabInteractable.trackRotation = false;
-
-            Debug.Log("OBJETO BLOQUEADO");
-        }
-    }
-
-    void CheckBothHands()
-    {
-        if(leftGrab && rightGrab)
-        {
-            Debug.Log("DOS CONTROLES DETECTADOS");
-
-            // Desbloquear movimiento
-            grabInteractable.trackPosition = true;
-
-            grabInteractable.trackRotation = true;
         }
     }
 }
